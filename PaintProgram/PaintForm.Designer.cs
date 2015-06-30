a89609bd610024fda47bb4362eb48f7d572db817
dsestak777 <mrdavey777@gmail.com>
2015-06-30 17:28:39 -0400

    Commit Files

---
+++ PaintProgram/PaintForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PaintProgram
+{
+    partial class PaintForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.colorLabel = new System.Windows.Forms.Label();
+            this.brushLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(196, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 410);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 185);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Color";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(13, 105);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(63, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Black";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.groupBox1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(13, 77);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(69, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Green";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.groupBox1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 49);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Blue";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.groupBox1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Red";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.groupBox1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton7);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Location = new System.Drawing.Point(0, 183);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 253);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Size";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(13, 77);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(66, 21);
+            this.radioButton7.TabIndex = 2;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Large";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.groupBox2_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(13, 50);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(78, 21);
+            this.radioButton6.TabIndex = 1;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Medium";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.groupBox2_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Checked = true;
+            this.radioButton5.Location = new System.Drawing.Point(13, 22);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(63, 21);
+            this.radioButton5.TabIndex = 0;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Small";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.groupBox2_CheckedChanged);
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.AutoSize = true;
+            this.colorLabel.ForeColor = System.Drawing.Color.Blue;
+            this.colorLabel.Location = new System.Drawing.Point(206, 417);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(0, 17);
+            this.colorLabel.TabIndex = 3;
+            // 
+            // brushLabel
+            // 
+            this.brushLabel.AutoSize = true;
+            this.brushLabel.ForeColor = System.Drawing.Color.Blue;
+            this.brushLabel.Location = new System.Drawing.Point(334, 417);
+            this.brushLabel.Name = "brushLabel";
+            this.brushLabel.Size = new System.Drawing.Size(0, 17);
+            this.brushLabel.TabIndex = 4;
+            // 
+            // PaintForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 433);
+            this.Controls.Add(this.brushLabel);
+            this.Controls.Add(this.colorLabel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "PaintForm";
+            this.Text = "DrawingProgram";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label colorLabel;
+        private System.Windows.Forms.Label brushLabel;
+
+    }
+}
+
---
+++ PaintProgram/PaintForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PaintProgram
+{
+    public partial class PaintForm : Form
+    {
+        //class variables
+        private bool canPaint = false;
+        Color color;
+        int thickness;
+        Bitmap image;
+        int x0;
+        int y0;
+       
+
+        public PaintForm()
+        {
+            InitializeComponent();
+            //initialize event handlers
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+            pictureBox1.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+            pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+            
+            //set default color & thickness
+            thickness = 5;
+            color = Color.Red;
+
+            //set default text
+            colorLabel.Text = color.ToString();
+            brushLabel.Text = "Brush Size: " + thickness;
+
+            //create new bitmap to hold picture data this will allow us to save data 
+            image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if mouse is pushed down paint
+            if (canPaint)
+            {
+                if (x0 != 0 && y0 != 0)
+                {
+                
+                    //create graphics object from bitmap
+                    Graphics g = Graphics.FromImage(image);
+
+                    //create a pen 
+                    Brush brush = new SolidBrush(color);
+                    Pen pen = new Pen(brush, thickness);
+                    pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                    pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                   
+                    //set start/finish points of line
+                    Point startPoint = new Point(x0, y0);
+                    Point endPoint = new Point(e.X, e.Y);
+                    g.DrawLine(pen, startPoint, endPoint);
+                    
+
+                    //make picture box image our bitmap
+                    pictureBox1.Image = image;
+                    g.Dispose();
+                }
+
+                //set previous mouse location from current location
+                x0 = e.X;
+                y0 = e.Y;
+
+            }
+        }
+
+       
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //set to true so we can paint
+            canPaint = true;
+
+            //set initial mouse draw location 
+            x0 = e.X;
+            x0 = e.Y;
+            pictureBox1_MouseMove(sender, e);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //set to false to we cannot paint
+            canPaint = false;
+
+            //set to zero
+            x0 = 0;
+            y0 = 0;
+        }
+
+        private void groupBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //determine which color radion button is selected & set color
+            if (sender == radioButton1)
+            {
+                color = Color.Red;
+            }
+            else if (sender == radioButton2)
+            {
+                color = Color.Blue;
+            }
+            else if (sender == radioButton3)
+            {
+                color = Color.Green;
+            }
+            else if (sender == radioButton4)
+            {
+                color = Color.Black;
+            }
+            //set color text label
+            colorLabel.Text = color.ToString(); 
+
+        }
+
+        private void groupBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //determine which size radio button is selected and set pen thickness
+            if (sender == radioButton5)
+            {
+                thickness = 5;
+            }
+            else if (sender == radioButton6)
+            {
+                thickness = 10;
+            }
+            else if (sender == radioButton7)
+            {
+                thickness = 15;
+            }
+            //set brush size label text
+            brushLabel.Text = "Brush Size: " + thickness;
+
+        }
+
+     
+    }
+}
